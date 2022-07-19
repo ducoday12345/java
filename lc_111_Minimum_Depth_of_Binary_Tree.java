cfaada01aceb38f17379b4513bd6f1a280e41ddf
Duc Minh Nguyen <104491261+ducoday12345@users.noreply.github.com>
2022-07-18 23:15:48 -0400

    Add files via upload

---
+++ lc_111_Minimum_Depth_of_Binary_Tree.java
@@ -0,0 +1,32 @@
+public class lc_111_Minimum_Depth_of_Binary_Tree {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public int minDepth(TreeNode root) {
+            // if(root == null) return 0;
+            // if(root.left == null|| root.right==null){
+            //     return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+            // }
+            // return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+            if(root == null) return 0;
+            if(root.left == null && root.right==null) return 1;
+            if(root.left == null) return 1+ minDepth(root.right);
+            if(root.right == null) return 1+ minDepth(root.left);
+            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        }
+
+    }
+}
